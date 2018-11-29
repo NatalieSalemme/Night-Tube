52a559f234337d0d56e3fe53b20b4079e822a1b9
Natalie Salemme <NatalieSalemme@gmail.com>
2018-11-28 17:38:45 -0800

    adding search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,22 +3,32 @@ import './App.css';
 
 class SearchBar extends React.Component {
   state = {
-    searchTerm: ''
+    searchTerm: '',
+    videos: []
   }
   onInputChange = e => {
     this.setState({
       searchTerm: e.target.value
     });
   }
+  onFormSubmit = e => {
+    e.preventDefault();
+
+  }
   render() {
     return (
-      <div className="ui fluid icon input search-bar">
-        <input
-          onChange={this.onInputChange}
-          type="text"
-          value={this.state.searchTerm}
-          placeholder="Search videos..." />
-        <i className="search icon"></i>
+      <div className="ui form search-container">
+        <form
+          onSubmit={this.onFormSubmit}>
+          <div className="ui icon input search-bar">
+            <input
+              onChange={this.onInputChange}
+              type="text"
+              value={this.state.searchTerm}
+              placeholder="Search videos..." />
+            <i className="search icon"></i>
+          </div>
+        </form>
       </div>
     );
   }
